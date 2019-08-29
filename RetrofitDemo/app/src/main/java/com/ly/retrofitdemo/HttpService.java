71f63633bfd8c79adf5cba7814b59fa43219bc74
ly <383209920@qq.com>
2019-08-29 16:14:34 +0800

    增加post请求

--- RetrofitDemo/app/src/main/java/com/ly/retrofitdemo/HttpService.java
+++ RetrofitDemo/app/src/main/java/com/ly/retrofitdemo/HttpService.java
@@ -3,7 +3,12 @@ package com.ly.retrofitdemo;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -17,4 +22,43 @@ import retrofit2.http.Query;
 public interface HttpService {
     @GET("ajax.php")
     Call<Translation> getCall(@Query("a") String a, @Query("f") String f, @Query("t") String t, @Query("w") String w);
+
+    /*
+    // 1.新闻类别
+    // 请求头：Content-Type:application/x-www-form-urlencoded
+    String newsCategories = "http://api.shujuzhihui.cn/api/news/categories?appKey=be3ac46843914f5cbe875defccd8f392";
+
+    -----------------------------------------------------------------------------------------------------------------
+
+    // 2.新闻列表
+    // 请求头：Content-Type:application/json
+    // 请求体：
+    {
+        "category": "要闻",
+        "page": "1"
+    }
+    String newsList = "http://api.shujuzhihui.cn/api/news/list?appKey=be3ac46843914f5cbe875defccd8f392";
+
+    -----------------------------------------------------------------------------------------------------------------
+
+    // 3.新闻详情
+    // 请求头（header）：Content-Type:application/x-www-form-urlencoded
+    // 请求内容（body）：newsId:82264991a5932770734bbd386aebedf6
+    {
+       "appKey": "be3ac46843914f5cbe875defccd8f392",
+       "newsId": "82264991a5932770734bbd386aebedf6"
+    }
+
+    String newsDetail = "http://api.shujuzhihui.cn/api/news/detail?appKey=be3ac46843914f5cbe875defccd8f392";
+    */
+    // 新闻详情
+    @POST("detail")
+    @FormUrlEncoded
+    @Headers("Content-Type:application/x-www-form-urlencoded")
+    Call<ResponseBody> getDetail(@Field("appKey") String appKey, @Field("newsId") String newsId);
+
+    // 新闻列表
+    @POST("list?appKey=be3ac46843914f5cbe875defccd8f392")
+    @Headers("Content-Type:application/json")
+    Call<ResponseBody> getList(@Body RequestBody requestBody);
 }
--- RetrofitDemo/app/src/main/java/com/ly/retrofitdemo/MainActivity.java
+++ RetrofitDemo/app/src/main/java/com/ly/retrofitdemo/MainActivity.java
@@ -8,6 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.MediaType;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -18,10 +22,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     //  http://fy.iciba.com/ajax.php?a=fy&f=auto&t=auto&w=helloworld
-    String BASE_URL = "http://fy.iciba.com/";
+    String BASE_URL_GET = "http://fy.iciba.com/";
+
+    String BASE_URL_POST = "http://api.shujuzhihui.cn/api/news/";
     Button get;
     Button post;
-    Button file;
     TextView text;
 
     @Override
@@ -30,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         get = findViewById(R.id.get);
         post = findViewById(R.id.post);
-        file = findViewById(R.id.file);
         text = findViewById(R.id.text);
 
         bindEvent();
@@ -41,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick(View view) {
                Retrofit retrofit = new Retrofit.Builder()
-                       .baseUrl(BASE_URL)
+                       .baseUrl(BASE_URL_GET)
                        .addConverterFactory(GsonConverterFactory.create())
                        .build();
                HttpService service =  retrofit.create(HttpService.class);
@@ -50,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
                call.enqueue(new Callback<Translation>() {
                    @Override
                    public void onResponse(Call<Translation> call, final Response<Translation> response) {
-                       Log.d("lylog","response =  "+response.body().getContent().getOut());
+                       Log.d("lylog","get response =  "+response.body().getContent().getOut());
                        text.post(new Runnable() {
                            @Override
                            public void run() {
@@ -61,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
 
                    @Override
                    public void onFailure(Call<Translation> call, Throwable t) {
-                       Log.d("lylog","onFailure ");
+                       Log.d("lylog","get onFailure t ="+t.toString());
                    }
                });
            }
@@ -70,16 +74,66 @@ public class MainActivity extends AppCompatActivity {
         post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(BASE_URL_POST)
+                        .build();
+                HttpService service =  retrofit.create(HttpService.class);
 
+                MediaType mediaType = MediaType.parse("application/json;charset=UTF-8");
+
+                // 获取到请求内容
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("category", "要闻");
+                    jsonObject.put("page", "1");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                String str = jsonObject.toString();
+
+                // 获取到请求体（RequestBody）
+                final RequestBody requestBody = RequestBody.create(mediaType, str);
+
+                // 3.获取call对象
+                final Call<ResponseBody> call = service.getList(requestBody);
+                call.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, final Response<ResponseBody> response) {
+                        Log.d("lylog","post response =  "+response);
+                        text.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                text.setText(response.toString());
+                            }
+                        });
+                    }
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Log.d("lylog","post onFailure t= "+t.toString());
+                    }
+                });
+
+
+//                final Call<ResponseBody> call = service.getDetail("be3ac46843914f5cbe875defccd8f392", "82264991a5932770734bbd386aebedf6");
+//                call.enqueue(new Callback<ResponseBody>() {
+//                    @Override
+//                    public void onResponse(Call<ResponseBody> call, final Response<ResponseBody> response) {
+//                        Log.d("lylog","post response =  "+response);
+//                        text.post(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                text.setText(response.toString());
+//                            }
+//                        });
+//                    }
+//                    @Override
+//                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+//                        Log.d("lylog","post onFailure t= "+t.toString());
+//                    }
+//                });
             }
         });
 
-        file.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
 
     }
 
